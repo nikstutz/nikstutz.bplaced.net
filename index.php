db91c82ecb58b94a908ee25e1568fb731c439b75
nikstutz <nik.stutz@edu.tbz.ch>
2021-02-04 16:54:16 +0100

    New file

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+header("Location: http://www.google.com/"); /* Browser umleiten */
+
+/* Stellen Sie sicher, dass der nachfolgende Code nicht ausgefuehrt wird, wenn
+   eine Umleitung stattfindet. */
+exit;
+?>
